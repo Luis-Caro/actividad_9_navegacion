f28a7136c4b8e126edb292014913622eda29e3c0
Luis-Caro <98632347+Luis-Caro@users.noreply.github.com>
2022-03-18 23:03:08 -0700

    Update DetailScreen.js

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -8,8 +8,8 @@ export default function SettingsScreen({ route, navigation }) {
    const getPost = async() => {
     try{
       const url = `https://jsonplaceholder.typicode.com/posts/${itemId}`;
-      const response = await fetch(url); //Aqui dice el profe que se consumen los datos
-      const json = await response.json(); //Aqui se convierte a Json en palabras del profe
+      const response = await fetch(url); //Aqui se consumen los datos
+      const json = await response.json(); //Aqui se convierte a Json
       setPost(json);
     } catch (error) {
 
