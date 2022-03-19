7d9b3720962e577354ed503b8623381e363d1b37
Luis Caro <a1121250026.COMtch.edu.mx>
2022-03-18 22:07:20 -0700

    actividad 9

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { View, Text, Button, SafeAreaView } from "react-native";
 
 export default function SettingsScreen({ route, navigation }) {
    const { itemId } = route.params;
+   const [post, setPost] = useState([]);
+
+   const getPost = async() => {
+    try{
+      const url = `https://jsonplaceholder.typicode.com/posts/${itemId}`;
+      const response = await fetch(url);
+      const json = await response.json();
+      setPost(json);
+    } catch (error) {
+
+      console.error(error);
+    }
+  }
+
+  useEffect(()=>{
+    getPost();
+  },[post])
+
+
+
+  
+
+
   return (
     <SafeAreaView>
-      <Text>Detalle {JSON.stringify(itemId)}</Text>
+      <Text>Id: {JSON.stringify(itemId)}</Text>
+      <Text>Titulo: {post.title}</Text>
+      <Text>Body: {post.body}</Text>
       <Button onPress={() =>{
         navigation.navigate('Home');
-      }} title="REGRESAR" />
+      }} title="Regresar" />
     </SafeAreaView>
   );
 }
